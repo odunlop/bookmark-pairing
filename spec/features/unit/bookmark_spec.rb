20f8f37d8979a97130d70470281ee3cf252ba523
Orla <orladunlop2@gmail.com>
2022-01-18 15:45:04 +0000

    Setting up test environment in project

--- spec/features/unit/bookmark_spec.rb
+++ spec/features/unit/bookmark_spec.rb
@@ -3,6 +3,13 @@ require 'bookmark'
 describe Bookmark do 
   describe '.all' do
     it "lists all of the user's bookmarks" do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
       bookmarks = Bookmark.all
       
       expect(bookmarks).to include("http://www.makersacademy.com")
